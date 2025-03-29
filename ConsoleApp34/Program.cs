38473c7f65d61c1858ce1c5df56360f7614e4948
MINEDUCYT <MINEDUCYT@DESKTOP-GLJR0B3>
2025-03-28 21:33:12 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp34/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp34
+{
+	internal class Program
+	{
+		class Auto
+		{
+			public void Encender()
+			{
+				Console.WriteLine("El auto está encendido.");
+			}
+
+			public void Acelerar()
+			{
+				Console.WriteLine("El auto está en movimiento.");
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			
+			Auto miAuto = new Auto();
+
+			
+			miAuto.Encender();
+			miAuto.Acelerar();
+
+			
+			Console.ReadKey();
+		}
+	}
+}
